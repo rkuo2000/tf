a2cbe98624674008d62e4184beeafb9478f029a1
Richard Kuo <rkuo2000@gmail.com>
2020-01-07 16:59:56 +0800

    Add files via upload

---
+++ food11_train.py
@@ -0,0 +1,174 @@
+# USAGE : python train.py
+
+# set the matplotlib backend so figures can be saved in the background
+import matplotlib
+matplotlib.use("Agg")
+
+import os
+import pickle
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.layers import Dropout, Flatten, Dense, Input
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import SGD
+from sklearn.metrics import classification_report
+from imutils import paths
+import matplotlib.pyplot as plt
+
+def plot_training(H, N, plotPath):
+	# construct a plot that plots and saves the training history
+	plt.style.use("ggplot")
+	plt.figure()
+	plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+	plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+	plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+	plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+	plt.title("Training Loss and Accuracy")
+	plt.xlabel("Epoch #")
+	plt.ylabel("Loss/Accuracy")
+	plt.legend(loc="lower left")
+	plt.savefig(plotPath)
+
+BATCH_SIZE = 32
+NUM_CLASSES = 11
+# derive the paths to the training, validation, and testing directories
+train_dir = 'dataset/training'
+val_dir   = 'dataset/validation'
+test_dir  = 'dataset/evaluation'
+
+# determine the total number of image paths in training, validation, and testing directories
+totalTrain = len(list(paths.list_images(train_dir)))
+totalVal = len(list(paths.list_images(val_dir)))
+totalTest = len(list(paths.list_images(test_dir)))
+
+# initialize the training data augmentation object
+trainAug = ImageDataGenerator(
+	rotation_range=30,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+# initialize the validation/testing data augmentation object 
+#(which we'll be adding mean subtraction to)
+valAug = ImageDataGenerator()
+
+# define the ImageNet mean subtraction (in RGB order) and set the
+# the mean subtraction value for each of the data augmentation objects
+mean = np.array([123.68, 116.779, 103.939], dtype="float32")
+trainAug.mean = mean
+valAug.mean = mean
+
+# initialize the training generator
+trainGen = trainAug.flow_from_directory(
+	train_dir,
+	class_mode="categorical",
+	target_size=(224, 224),
+	color_mode="rgb",
+	shuffle=True,
+	batch_size=BATCH_SIZE)
+
+# initialize the validation generator
+valGen = valAug.flow_from_directory(
+	val_dir,
+	class_mode="categorical",
+	target_size=(224, 224),
+	color_mode="rgb",
+	shuffle=False,
+	batch_size=BATCH_SIZE)
+
+# initialize the testing generator
+testGen = valAug.flow_from_directory(
+	test_dir,
+	class_mode="categorical",
+	target_size=(224, 224),
+	color_mode="rgb",
+	shuffle=False,
+	batch_size=BATCH_SIZE)
+
+# load the VGG16 network, ensuring the head FC layer sets are left off
+baseModel = VGG16(weights="imagenet", include_top=False, input_shape=(224,224,3))
+
+# construct the head of the model that will be placed on top of the the base model
+headModel = baseModel.output
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(512, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(NUM_CLASSES, activation="softmax")(headModel)
+
+# place the head FC model on top of the base model (this will become the actual model we will train)
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+# loop over all layers in the base model and freeze them so they will *not* be updated during the first training process
+for layer in baseModel.layers:
+	layer.trainable = False
+
+# compile our model (this needs to be done after our setting our layers to being non-trainable
+print("[INFO] compiling model...")
+opt = SGD(lr=1e-4, momentum=0.9)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+model.summary()
+
+# train the head of the network for a few epochs (all other layers
+# are frozen) -- this will allow the new FC layers to start to become
+# initialized with actual "learned" values versus pure random
+print("[INFO] training head...")
+H = model.fit_generator(
+	trainGen,
+	steps_per_epoch=totalTrain // BATCH_SIZE,
+	validation_data=valGen,
+	validation_steps=totalVal // BATCH_SIZE,
+	epochs=50)
+
+# reset the testing generator and evaluate the network after
+# fine-tuning just the network head
+print("[INFO] evaluating after fine-tuning network head...")
+testGen.reset()
+predIdxs = model.predict_generator(testGen,	steps=(totalTest // BATCH_SIZE) + 1)
+predIdxs = np.argmax(predIdxs, axis=1)
+print(classification_report(testGen.classes, predIdxs, target_names=testGen.class_indices.keys()))
+plot_training(H, 50, 'food11_warmup_plot.png')
+
+# reset our data generators
+trainGen.reset()
+valGen.reset()
+
+# now that the head FC layers have been trained/initialized, 
+# lets unfreeze the final set of CONV layers and make them trainable
+for layer in baseModel.layers[15:]:
+	layer.trainable = True
+
+# loop over the layers in the model and show which ones are trainable or not
+for layer in baseModel.layers:
+	print("{}: {}".format(layer, layer.trainable))
+
+# for the changes to the model to take affect we need to recompile the model, this time using SGD with a *very* small learning rate
+print("[INFO] re-compiling model...")
+opt = SGD(lr=1e-4, momentum=0.9)
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+
+# train the model again, this time fine-tuning *both* the final set of CONV layers along with our set of FC layers
+H = model.fit_generator(
+	trainGen,
+	steps_per_epoch=totalTrain // BATCH_SIZE,
+	validation_data=valGen,
+	validation_steps=totalVal // BATCH_SIZE,
+	epochs=20)
+
+# reset the testing generator and then use our trained model to
+# make predictions on the data
+print("[INFO] evaluating after fine-tuning network...")
+testGen.reset()
+predIdxs = model.predict_generator(testGen,	steps=(totalTest // BATCH_SIZE) + 1)
+predIdxs = np.argmax(predIdxs, axis=1)
+print(classification_report(testGen.classes, predIdxs, target_names=testGen.class_indices.keys()))
+plot_training(H, 20, 'food11_final_plot.png')
+
+# serialize the model to disk
+print("[INFO] serializing network...")
+model.save('model/food11.h5')
