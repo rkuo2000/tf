6056c170165a33b11b2f3e394554ca67c5ce78ab
Richard Kuo <rkuo2000@gmail.com>
2021-09-14 20:06:16 +0800

    Add files via upload

---
+++ plot_images.py
@@ -0,0 +1,22 @@
+# pip install matplotlib
+# git clone https://github.com/rkuo2000/tf
+# cd tf
+import os
+import glob
+import matplotlib.pyplot as plt
+
+img_dir = './images'
+all_img_files = glob.glob(img_dir+'/*.jpg')
+
+img_files = all_img_files[:10]
+print(img_files)
+
+plt.figure(figsize=(10, 10))
+
+for idx, file_path in enumerate(img_files):
+    plt.subplot(5, 5, idx+1)
+
+    img = plt.imread(file_path)
+    plt.tight_layout()
+    plt.imshow(img, cmap='gray')
+plt.show()
