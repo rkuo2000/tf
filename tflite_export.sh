b0af1892706d60b1af4fd855025f02ce39eb5984
Richard Kuo <rkuo2000@gmail.com>
2021-09-16 04:25:48 +0800

    Delete tflite_export.sh

--- tflite_export.sh
+++
@@ -1,4 +0,0 @@
-### from model.ckpt to tflite_graph.pb & .pbtxt
-#cd ~/models/research/object_detection
-#./tflite_export.sh training
-python export_tflite_ssd_graph.py --pipeline_config_path $1/pipeline.config --trained_checkpoint_prefix $1/model.ckpt --output_directory $1 
