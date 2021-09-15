51cbe06ecc6c546fc5124bbd7650e3f213c10720
Richard Kuo <rkuo2000@gmail.com>
2021-09-16 04:24:57 +0800

    Delete tflite_convert_h5.sh

--- tflite_convert_h5.sh
+++
@@ -1,6 +0,0 @@
-### convert keras model .h5 into .tflite (for PC/Android)
-# cd ~/tf
-# ./tflite_convert_h5.sh tl_mobilenetv2
-tflite_convert \
---keras_model_file=model/$1.h5 \
---output_file=model/$1.tflite
