ca72e9a0554074b90e4cbba64b01328d05df2078
Richard Kuo <rkuo2000@gmail.com>
2021-09-16 11:14:35 +0800

    Add files via upload

---
+++ download_stock_quote.py
@@ -0,0 +1,11 @@
+# $pip install alpha_vantage
+# $python download_stock_quote.py SYMBOL
+import sys
+from alpha_vantage.timeseries import TimeSeries
+API_KEY = 'K6LJO3BJMBJMTT1A' # get it free from https://www.alphavantage.co/
+
+symbol = sys.argv[1]
+
+ts = TimeSeries(key=API_KEY, output_format='pandas')
+data, meta_data = ts.get_daily(symbol, outputsize='full')
+data.to_csv(f'./datasets/stock/{symbol}.csv')
