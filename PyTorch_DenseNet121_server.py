8fb9e99714cc67f3d8eeceff921cd50907e896dd
Richard Kuo <rkuo2000@gmail.com>
2021-10-29 14:57:46 +0800

    Add files via upload

---
+++ PyTorch_DenseNet121_server.py
@@ -0,0 +1,57 @@
+### PyTorch DenseNet121 server with Flask API
+#import torchvision.models as models
+#resnet18 = models.resnet18()
+#alexnet = models.alexnet()
+#vgg16 = models.vgg16()
+#squeezenet = models.squeezenet1_0()
+#densenet = models.densenet161()
+#inception = models.inception_v3()
+#googlenet = models.googlenet()
+#shufflenet = models.shufflenet_v2_x1_0()
+#mobilenet_v2 = models.mobilenet_v2()
+#mobilenet_v3_large = models.mobilenet_v3_large()
+#mobilenet_v3_small = models.mobilenet_v3_small()
+#resnext50_32x4d = models.resnext50_32x4d()
+#wide_resnet50_2 = models.wide_resnet50_2()
+#mnasnet = models.mnasnet1_0()
+
+import io
+import json
+
+from torchvision import models
+import torchvision.transforms as transforms
+from PIL import Image
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+imagenet_class_index = json.load(open('./models/imagenet_class_index.json'))
+model = models.densenet121(pretrained=True)
+model.eval()
+
+def transform_image(image_bytes):
+    my_transforms = transforms.Compose([transforms.Resize(255),
+                                        transforms.CenterCrop(224),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406],
+                                            [0.229, 0.224, 0.225])])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+	
+def get_prediction(image_bytes):
+    tensor = transform_image(image_bytes=image_bytes)
+    outputs = model.forward(tensor)
+    _, y_hat = outputs.max(1)
+    predicted_idx = str(y_hat.item())
+    return imagenet_class_index[predicted_idx]
+	
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        img_bytes = file.read()
+        class_id, class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_id': class_id, 'class_name': class_name})
+    
+if __name__ == '__main__':
+    app.run()
