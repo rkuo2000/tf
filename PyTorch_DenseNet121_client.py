20a585ff4183f78faf109b1daf753ce81fbf7df1
Richard Kuo <rkuo2000@gmail.com>
2024-04-18 12:41:57 +0800

    Delete PyTorch_DenseNet121_client.py

--- PyTorch_DenseNet121_client.py
+++
@@ -1,7 +0,0 @@
-# PyTorch DenseNet121 client 
-import requests
-
-resp = requests.post("http://localhost:5000/predict",
-                     files={"file": open('./images/cat.jpg','rb')})
-
-print(resp.text)
