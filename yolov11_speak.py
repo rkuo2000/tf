809a4dc4561a5c42e7b9b673d59b5721a0a77740
Richard Kuo <rkuo2000@gmail.com>
2024-10-09 09:41:04 +0800

    Add files via upload

---
+++ yolov11_speak.py
@@ -0,0 +1,41 @@
+## pip install ultralytics
+## pip install gtts
+# usage: python yolov11_speak.py images/traffic.jpg
+
+import os
+import sys
+from ultralytics import YOLO
+from gtts import gTTS
+
+if len(sys.argv) >1:
+    filename = sys.argv[1]
+else:
+    filename = "images/traffic.jpg"
+
+## Object Detection
+model = YOLO("yolo11n.pt")
+
+results = model(filename) # ,save-txt=True)
+
+results[0].show()
+
+## Object Counting
+labels = results[0].names
+
+cls = results[0].boxes.cls.tolist()
+
+unique = list(dict.fromkeys(cls))
+
+text = "There are "
+for label in unique:
+    count = cls.count(label)
+    text = text + str(count) + " " + labels[int(label)] + ","
+print(text)
+
+## TTS 
+sl = "en"
+tts = gTTS(text, lang=sl)
+tts.save("gTTS.mp3")
+
+## Speak
+os.system("mpg123 -q gTTS.mp3")
